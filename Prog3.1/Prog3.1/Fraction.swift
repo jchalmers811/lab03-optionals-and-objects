ad513b17794da117ec2c7b23a393f2a99ef814b7
James <jchalmers811@gmail.com>
2018-07-12 20:00:17 +1200

    Fraction class added and working with wrapper

---
+++ Prog3.1/Prog3.1/Fraction.swift
@@ -0,0 +1,313 @@
+//
+//  Fraction.swift
+//  Fractions
+//
+//  Represent a number as a fraction of two integers
+//
+//  Created by James on 11/07/18.
+//  Copyright © 2018 james. All rights reserved.
+//
+
+import Foundation
+
+class Fraction : CustomStringConvertible {
+    
+    private let num: Int;   // Numerator
+    private let den: Int;   // Denominator
+    
+    /**
+     Converts fraction to a Float value
+     
+     - returns: Float Decimal value of the fraction
+     */
+    var decimal: Float {
+        get {
+            // num and den are of type Int, therefore,
+            // they need to be explicitly converted to Floats
+            return Float(self.num)/Float(self.den);
+        }
+    }
+    
+    /**
+     Converts object to a string description
+     
+     - returns: String String representation of a fraction
+     */
+    var description: String {
+        if self.num == 0 {
+            return "0"
+        } else if self.den == 1 {
+            return "\(self.num)"
+        } else if self.num/self.den > 1 {
+            return "\(self.num/self.den) \(self.num%self.den)/\(self.den)"
+        }
+        
+        return "\(self.num)/\(self.den)"
+    }
+    
+    
+    
+    
+    /**
+        Default initialiser
+     
+        Initialise fraction to zero
+    */
+    convenience init() {
+        self.init(num: 0, den: 1)
+    }
+    
+    /**
+        Designated initialiser
+     
+    */
+    init(num : Int, den : Int) {
+        
+        // Check the denominator...
+        assert(den != 0, "Denominator cannot be zero")
+        
+        // Arguments are constants, redefine them
+        // as variables
+        var num = num;
+        var den = den;
+        
+        if(den < 0) {
+            // If the denominator is negative
+            // multiply the numerator and
+            // denominator by -1 - this
+            // ensures the denominator is
+            // always positive, and numerator
+            // carries the appropriate sign
+            num = -num
+            den = -den
+        }
+        
+        // Find greatest common denominator
+        for gcd in (1...den).reversed() {
+            if(num%gcd == 0 && den%gcd==0) {
+                // Common denominator found,
+                // divide numerator and denominator
+                num /= gcd
+                den /= gcd
+                break
+            }
+        }
+        
+        self.num = num
+        self.den = den
+    }
+    
+    /**
+        Convenience initialiser
+     
+    */
+    convenience init(num : Int) {
+        self.init(num: num, den: 1)
+    }
+    
+    
+    
+    // METHODS
+    
+    /**
+     Adds a fraction to self.
+     
+     - parameter f: Fraction to add to self
+     
+     - returns: Fraction The result of adding f to self.
+     */
+    func add(_ f: Fraction) -> Fraction {
+        return Fraction(num: self.num*f.den + self.den*f.num,
+                        den: self.den*f.den)
+    }
+    
+    /**
+     Subtracts fraction from self.
+     
+     - parameter f: Fraction to subtract from self
+     
+     - returns: Fraction The result of subtracting f from self.
+     */
+    func subtract(_ f: Fraction) -> Fraction {
+        return Fraction(num: self.num*f.den - self.den*f.num,
+                        den: self.den*f.den)
+    }
+    
+    /**
+     Multiplies self by a fraction.
+     
+     - parameter f: Fraction to multiply self by
+     
+     - returns: Fraction The result of multiplying self by f.
+     */
+    func multiply(_ f: Fraction) -> Fraction {
+        return Fraction(num: self.num*f.num, den: self.den*f.den)
+    }
+    
+    /**
+     Divides self by a fraction.
+     
+     - parameter f: Fraction to divide self by
+     
+     - returns: Fraction The result of dividing self by f.
+     */
+    func divide(_ f: Fraction) -> Fraction {
+        return Fraction(num: self.num*f.den, den: self.den*f.num)
+    }
+    
+    /**
+     Add a fraction to fraction.
+     
+     - parameter f1: Fraction to add to
+     - parameter f2: Fraction to be added
+     
+     - returns: The result of f1 + f2.
+     */
+    static func add(_ f1: Fraction, to f2: Fraction) -> Fraction {
+        return Fraction(num: f1.num*f2.den + f1.den*f2.num,
+                        den: f1.den*f2.den)
+    }
+    
+    /**
+     Subtract a fraction from fraction.
+     
+     - parameter f1: Fraction to subtract
+     - parameter f2: Fraction to subtract from
+     
+     - returns: The result of f2 - f1.
+     */
+    static func subtract(_ f1: Fraction, from f2: Fraction) -> Fraction {
+        return f2.subtract(f1);
+    }
+    
+    /**
+     Multiply a fraction by fraction.
+     
+     - parameter f1: Fraction to multiply
+     - parameter f2: Fraction to multiply by
+     
+     - returns: The result of f1*f2.
+     */
+    static func multiply(_ f1: Fraction, by f2: Fraction) -> Fraction {
+        return f1.multiply(f2)
+    }
+    
+    /**
+     Divide a fraction by fraction.
+     
+     - parameter f1: Fraction to divide
+     - parameter f2: Fraction to divide by
+     
+     - returns: The result of f1/f2.
+     */
+    static func divide(_ f1: Fraction, by f2: Fraction) -> Fraction {
+        return f1.divide(f2)
+    }
+    
+    /**
+     Adds an integer to self.
+     
+     - parameter x: Integer to add to self
+     
+     - returns: Fraction The result of adding x to self.
+     */
+    func add(_ x: Int) -> Fraction {
+        return Fraction(num: self.num + self.den*x,
+                        den: self.den)
+    }
+    
+    /**
+     Subtracts an integer from self.
+     
+     - parameter x: Integer to subtract from self
+     
+     - returns: Fraction The result of subtracting x from self.
+     */
+    func subtract(_ x: Int) -> Fraction {
+        return Fraction(num: self.num - self.den*x,
+                        den: self.den)
+    }
+    
+    /**
+     Multiplies self by an integer.
+     
+     - parameter x: Integer to multiply self by
+     
+     - returns: Fraction The result of multiplying self by x.
+     */
+    func multiply(_ x: Int) -> Fraction {
+        return Fraction(num: self.num*x, den: self.den)
+    }
+    
+    /**
+     Divides self by an integer.
+     
+     - parameter x: Integer to divide self by
+     
+     - returns: Fraction The result of dividing self by x.
+     */
+    func divide(_ x: Int) -> Fraction {
+        return Fraction(num: self.num, den: self.den*x)
+    }
+
+    
+}
+
+/**
+ + operator between two Fractions
+ */
+func +(f1: Fraction, f2: Fraction) -> Fraction {
+    return f1.add(f2)
+}
+
+/**
+ - operator between two Fractions
+ */
+func -(f1: Fraction, f2: Fraction) -> Fraction {
+    return f1.subtract(f2)
+}
+
+/**
+ * operator between two Fractions
+ */
+func *(f1: Fraction, f2: Fraction) -> Fraction {
+    return f1.multiply(f2)
+}
+
+/**
+ / operator between two Fractions
+ */
+
+func /(f1: Fraction, f2: Fraction) -> Fraction {
+    return f1.divide(f2)
+}
+
+/**
+ + operator between two Fractions
+ */
+func +(f: Fraction,  i: Int) -> Fraction {
+    return f.add(Fraction(num: i))
+}
+
+/**
+ - operator between two Fractions
+ */
+func -(f: Fraction, i: Int) -> Fraction {
+    return f.subtract(Fraction(num: i))
+}
+
+/**
+ * operator between two Fractions
+ */
+func *(f: Fraction, i: Int) -> Fraction {
+    return f.multiply(Fraction(num: i))
+}
+
+/**
+ / operator between two Fractions
+ */
+
+func /(f: Fraction, i: Int) -> Fraction {
+    return f.divide(Fraction(num: i))
+}
+
--- Prog3.1/Prog3.1/main.swift
+++ Prog3.1/Prog3.1/main.swift
@@ -40,3 +40,29 @@ if x != NIL {
 } else {
     print("x has no value")
 }
+
+//Declare vriable z as a SimpleOptional
+var z: SimpleOptional
+
+//Assign a Fraction object 1/3 to z
+z = SimpleOptional(value: Fraction(num: 1, den: 3))
+
+//Print z
+print("z=\(z)")
+
+//Check if z has a value or not
+if z != NIL {
+    //If z has a value, unwrap that value
+    //and print it
+    print("z has a value of \(z.unwrap)")
+    //If the value of an optional is an object,
+    //the optional must be unwrapped in order
+    //to access the properties and methods of that object
+    
+    //Unwrap z and force type cast it to a Fraction object
+    let v = z.unwrap as! Fraction
+    //Print the computed decimal property
+    print("The decimal property of z's value is \(v.decimal)")
+} else {
+    print("z has no value")
+}
